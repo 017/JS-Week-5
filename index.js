cc2d166c9d4a9d8fd6ac2df6b3079cb9f355e986
Robert Ochi <robeochi@gmail.com>
2020-10-13 00:37:28 -0600

    initial commit.

---
+++ index.js
@@ -0,0 +1,283 @@
+class Player {
+  constructor(playerName, playerClass, STR, AGI, CON, CHA, WIS, INT, HP, AC) {
+    this.playerName = playerName;
+    this.playerClass = playerClass;
+    this.strength = STR;
+    this.agility = AGI;
+    this.constitution = CON;
+    this.charisma = CHA;
+    this.wisdom = WIS;
+    this.intelligence = INT;
+    this.hitPoints = HP;
+    this.armorClass = AC;
+  }
+
+  rollStat() {
+    result = Math.floor((Math.random() * 6) + (Math.random() * 6) + (Math.random() * 6));
+    return result;
+  }
+
+  getRollMod(stat) {
+    if (stat >= 18) {
+      return 5;
+    } else if (stat >= 16) {
+      return 4;
+    } else if (stat >= 14) {
+      return 3;
+    } else if (stat >= 12) {
+      return 2;
+    } else if (stat >= 10) {
+      return 1;
+    } else if (stat >= 8) {
+      return 0;
+    } else if (stat >= 6) {
+      return -1;
+    } else if (stat >= 4) {
+      return -2;
+    } else if (stat >= 2) {
+      return -3;
+    } else {
+      return -4;
+    }
+  }
+
+  rollHP(hpstat) {
+    let rolledHitPoints = Math.floor((Math.random() * 6) * this.getRollMod(hpstat));
+    return rolledHitPoints;
+  }
+
+  getAC(stat) {
+    let calculatedAC = 10 + this.getRollMod(stat);
+    return calculatedAC;
+  }
+
+  describe() {
+    let result = alert(`
+    Player Details:
+    Name: ${this.playerName}
+    Class: ${this.playerClass}
+    AC: ${this.armorClass}
+    HP: ${this.hitPoints}
+    Constitution: ${this.constitution} (${this.getRollMod(this.constitution)})
+    Strength: ${this.strength} (${this.getRollMod(this.strength)})
+    Agility: ${this.agility} (${this.getRollMod(this.agility)})
+    Charisma: ${this.charisma} (${this.getRollMod(this.charisma)})
+    Wisdom: ${this.wisdom} (${this.getRollMod(this.wisdom)})
+    Intelligence: ${this.intelligence} (${this.getRollMod(this.intelligence)})
+    `);
+    return result;
+  }
+}
+
+class Team {
+  constructor(name) {
+    this.name = name;
+    this.players = [];
+  }
+
+  addPlayer(ply) {
+    if (ply instanceof Player) {
+      this.players.push(ply);
+    } else {
+      throw new Error(`You can only add an instance of player. This is not a player: ${ply}`);
+    }
+  }
+
+  describe() {
+    let result = `[${this.name}] has ${this.players.length} players.`;
+    return result;
+  }
+}
+
+class Menu {
+  constructor() {
+    this.teams = [];
+    this.selectedTeam = null;
+  }
+
+  start() {
+    let selection = this.showMainMenu();
+    while (selection != 0) {
+      switch (selection) {
+        case '1':
+          this.createTeam();
+          break;
+        case '2':
+          this.viewTeam();
+          break;
+        case '3':
+          this.deleteTeam();
+          break;
+        case '4':
+          this.displayTeams();
+          break;
+        default:
+          selection = 0;
+      }
+      selection = this.showMainMenu();
+    }
+
+    alert('Closing Menu!');
+  }
+
+  showMainMenu() {
+    return prompt(`
+    -----------------------
+    0: EXIT
+    1: CREATE NEW TEAM
+    2: VIEW TEAM
+    3: DELETE TEAM
+    4: DISPLAY ALL TEAMS
+    -----------------------
+    `);
+  }
+
+  showTeamMenu(teamInfo) {
+    return prompt(`
+    -----------------------
+    0: BACK
+    1: CREATE PLAYER
+    2: DELETE PLAYER
+    -----------------------
+    `);
+  }
+
+  createTeam() {
+    let name = prompt(`Please enter a name for your new team.`);
+    this.teams.push(new Team(name));
+  }
+
+  deleteTeam() {
+    let target = prompt(`Enter the name of the team to delete.`);
+    let index = this.teams.indexOf(target);
+    if (index !== -1) {
+      this.selectedTeam.players.splice(index, 1);
+    }
+  }
+
+  viewTeam() {
+    let searchName = prompt(`Enter the name of the team you want to view:
+    ${this.teams}`);
+    let index = this.teams.indexOf(searchName); //Search for the result
+    if (index !== -1) {
+      this.selectedTeam = this.teams[index];
+      let description = `Team Name: ${this.selectedTeam.name}
+      
+      `;
+
+      for (let i = 0; i < this.selectedTeam.players.length; i++) {
+
+        description += `[${i}] ${this.selectedTeam.players[i].playerName}
+        Class: ${this.selectedTeam.players[i].playerClass} 
+        HP: ${this.selectedTeam.players[i].hitPoints}
+        AC: ${this.selectedTeam.players[i].armorClass}
+        `;
+      }
+
+      let selection = this.showTeamMenu(description);
+      switch (selection) {
+        case '1':
+          this.createPlayer();
+          break;
+        case '2':
+          this.deletePlayer();
+      }
+
+    } else if (result === -1) {
+      throw new Error(`Error: Provided input was not found. Please try again with a different name.`);
+    } else {
+      throw new Error(`Error: Invalid Input`);
+    }
+  }
+
+  createPlayer() {
+    let validClasses = ['Fighter', 'Rogue', 'Ranger', 'Wizard', 'Sorcerer', 'Warlock', 'Paladin', 'Barbarian'];
+    let playerName = prompt('Enter new player name. Stats will be randomized for the new player.');
+    let STR = Player.rollStat();
+    let AGI = Player.rollStat();
+    let CON = Player.rollStat();
+    let CHA = Player.rollStat();
+    let WIS = Player.rollStat();
+    let INT = Player.rollStat();
+    let playerClass = prompt(`
+    Select a new player class.
+    This can be: ${validClasses}
+
+    Your stats:
+    Strength: ${STR}
+    Agility: ${AGI}
+    Constitution: ${CON}
+    Charisma: ${CHA}
+    Dexterity: ${DEX}
+    Wisdom: ${WIS}
+    Intelligence: ${INT}
+    `);
+
+    let HP;
+    if (playerClass == 'Barbarian') {
+      HP = Player.rollHP(CON * 2);
+    } else {
+      HP = Player.rollHP(CON);
+    }
+    alert(`HitPoints: ${HP}`);
+
+    let AC;
+    if (playerClass == 'Paladin', 'Warlock') {
+      AC = Player.getAC(CHA);
+    } else {
+      AC = Player.getAC(AGI);
+    }
+
+    function validateClass(name) {
+      validClasses.forEach(function (name) {
+        if (validClasses.indexOf(name) === -1) {
+          return false;
+        } else {
+          return true;
+        }
+      });
+    }
+
+    if (validateClass(playerClass) === true) {
+      this.selectedTeam.players.push(new Player(playerName, playerClass, STR, AGI, CON, CHA, WIS, INT, HP, AC));
+    } else {
+      throw new Error(`Your provided classname is invalid. Please try again.`);
+    }
+
+  }
+
+  deletePlayer() {
+    let target = prompt(`Enter the name of the player to delete.`);
+    let index = this.selectedTeam.players.indexOf(target);
+    if (index !== -1) {
+      this.selectedTeam.players.splice(index, 1);
+    }
+  }
+
+  displayTeams() {
+    let teamString = '';
+    for (let k = 0; k < this.teams.length; k++) {
+      teamString += `[${k}] ${this.teams[k].name}
+      `; // Return 1 line each time at the end
+    }
+    alert(teamString);
+  }
+
+
+
+}
+
+let menu = new Menu();
+console.log(menu);
+console.log(menu.start);
+
+// this.players = [
+//   new Player('Oryn', 'Fighter', 'The Crimson Blades', 18, 13, 15, 8, 10, 12, 45, 15),
+// ];
+
+// Constitution: ${this.selectedTeam.players[i].constitution} (+${Player.getRollMod(this.selectedTeam.players[i].constitution)})
+// Strength: ${this.selectedTeam.players[i].strength} (+${Player.getRollMod(this.selectedTeam.players[i].strength)})
+// Agility: ${this.selectedTeam.players[i].agility} (+${Player.getRollMod(this.selectedTeam.players[i].agility)})
+// Charisma: ${this.selectedTeam.players[i].charisma} (+${Player.getRollMod(this.selectedTeam.players[i].charisma)})
+// Wisdom: ${this.selectedTeam.players[i].wisdom} (+${Player.getRollMod(this.selectedTeam.players[i].wisdom)})
+// Intelligence: ${this.selectedTeam.players[i].intelligence} (+${Player.getRollMod(this.selectedTeam.players[i].intelligence)})
